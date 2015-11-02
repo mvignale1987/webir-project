c86ee1366f41b453084824731f20c0e5b4b45dcb
Mauricio Vignale <mvignale1987@gmail.com>
2015-11-02 17:51:15 -0300

    limpio un cacho de codigo

--- webir-webapp/express/controller.js
+++ webir-webapp/express/controller.js
@@ -2,12 +2,16 @@ var Noticia = require('./models/noticia');
 
 // Obtiene todos los objetos Persona de la base de datos
 exports.getNoticias = function (req, res){
-	Noticia.find(
+  // Aca hay que fijarse solamente en los query.parameters y ver
+  // si existen, para ver que find hacemos, una papita: EXAMPLE
+  // res.send("tagId is set to " + req.query.tagId);
+
+  Noticia.find(
 		function(err, noticia) {
 			if (err) {
         res.send(err)
-        }
+      }
 			res.json(noticia); // devuelve todas las Noticias en JSON
-			}
-		);
+		}
+	);
 }
--- webir-webapp/express/routes.js
+++ webir-webapp/express/routes.js
@@ -5,13 +5,7 @@ module.exports = function(app) {
 
 	// devolver todos los Personas
 	app.get('/noticias', Controller.getNoticias);
-	// // Crear una nueva Persona
-	// app.post('/noticia', Controller.setNoticia);
-	// // Modificar los datos de una Persona
-	// app.put('/api/persona/:persona_id', Controller.updatePersona);
-	// // Borrar una Persona
-	// app.delete('/api/persona/:persona_id', Controller.removePersona);
-
+	
   app.get('/', function(req, res){
   	res.sendfile('./public/index.html');
   });

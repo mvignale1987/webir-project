d42ec2199a69a340a827970d90b2352d042e3706
Mauricio Vignale <mvignale1987@gmail.com>
2015-11-03 14:55:22 -0300

    API de noticias contra mongo completa los metodos son: 1) sitio=.... busca por sitio 2) palabra="" lista ENTRE COMILLAS de palabras, se buscan en mongo con AND concatenadas 3) fechaDesde=YYYY-MM-DD dia desde el cual buscar las noticias, formato año-mes-dia 4) fechaHasta=YYYY-MM-DD analogo a fechaDesde pero fecha limite
    Todos los atributos se pueden combinar en una sola query con ? y &
    
    ejemplo
    
    localhost:3000/noticias?palabra="enano odalisca disfraza"&fechaHasta=2016-7-25&fechaDesde=2013-7-30
    
    esta con el export que les pase deberia traerles un conjunto de noticias

--- webir-webapp/express/controller.js
+++ webir-webapp/express/controller.js
@@ -2,16 +2,28 @@ var Noticia = require('./models/noticia');
 
 // Obtiene todos los objetos Persona de la base de datos
 exports.getNoticias = function (req, res){
-  // Aca hay que fijarse solamente en los query.parameters y ver
-  // si existen, para ver que find hacemos, una papita: EXAMPLE
-  // res.send("tagId is set to " + req.query.tagId);
+    var queryParams = {};
+    if (req.query.sitio)
+      queryParams.sitio = req.query.sitio;
+    if (req.query.palabra)
+      queryParams.$text = { $search: req.query.palabra.replace(/ /g,"\" \"") };
+    queryParams.fecha = {};
+    if (req.query.fechaDesde){
+      var fechaDesde = req.query.fechaDesde.split("-");
+      queryParams.fecha.$gte = new Date(fechaDesde[0], fechaDesde[1], fechaDesde[2]);
 
-  Noticia.find(
-		function(err, noticia) {
-			if (err) {
-        res.send(err)
-      }
-			res.json(noticia); // devuelve todas las Noticias en JSON
-		}
-	);
+    }if (req.query.fechaHasta) {
+      var fechaHasta = req.query.fechaHasta.split("-");
+      queryParams.fecha.$lte = new Date(fechaHasta[0], fechaHasta[1], fechaHasta[2]);
+    }
+
+    Noticia.find(queryParams,
+  		function(err, noticia) {
+  			if (err) {
+          res.send(err)
+        }
+  			res.json(noticia); // devuelve todas las Noticias en JSON
+  		}
+  	);
+
 }
